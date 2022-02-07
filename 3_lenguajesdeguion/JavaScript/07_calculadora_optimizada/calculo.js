3d2ab18adabe89ad5e4c06f0aae3bffcc62a8cd2
ElenaRgC <95099947+ElenaRgC@users.noreply.github.com>
2022-02-07 19:40:31 +0100

    Optimized calculator

---
+++ 3_lenguajesdeguion/JavaScript/07_calculadora_optimizada/calculo.js
@@ -0,0 +1,23 @@
+function calculo(operacion) {
+  var valor1 = Number(document.getElementById("valor1").value);
+  var valor2 = Number(document.getElementById("valor2").value);
+
+  if (operacion == "s") {
+    resultado = valor1 + valor2;
+  } else if (operacion == "r") {
+    resultado = valor1 - valor2;
+  } else if (operacion == "m") {
+    resultado = valor1 * valor2;
+  } else if (operacion == "d") {
+    resultado = valor1 / valor2;
+  }
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function clear() {
+  document.getElementById("valor1").value = " ";
+  document.getElementById("valor2").value = " ";
+  document.getElementById("resultado").value = " ";
+  document.getElementById("valor1").focus();
+}

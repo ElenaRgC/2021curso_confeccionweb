ffbccd54502a0b85db28c1ae17c4d0fc6b1ac4d5
ElenaRgC <95099947+ElenaRgC@users.noreply.github.com>
2022-02-10 20:20:31 +0100

    Complete exercise

--- 3_lenguajesdeguion/JavaScript/11_formulario/script.js
+++ 3_lenguajesdeguion/JavaScript/11_formulario/script.js
@@ -1,3 +1,60 @@
+function comprobarDNI() {
+  var dniUsuario,
+    letraDNI,
+    numeroDNI = "",
+    letras = "TRWAGMYFPDXBNJZSQVHLCKE",
+    resto = 0;
+
+  dniUsuario = document.getElementById("dni").value;
+
+  letraDNI = dniUsuario[8];
+
+  for (i = 0; i < dniUsuario.length - 1; i++) {
+    numeroDNI = numeroDNI + dniUsuario[i];
+  }
+
+  numeroDNI = Number(numeroDNI);
+
+  resto = numeroDNI % 23;
+
+  if (letras[resto] != letraDNI) {
+    document.getElementById("dni").classList.add("rojo");
+  } else {
+    document.getElementById("dni").classList.remove("rojo");
+  }
+}
+
+function codigoPostal() {
+  var cPostalUsuario,
+    localidadSeleccionada,
+    localidadSeleccionadaIndice,
+    localidadSeleccionadaValor;
+
+  localidadSeleccionada = document.getElementById("localidad");
+  localidadSeleccionadaIndice = localidadSeleccionada.selectedIndex;
+  localidadSeleccionadaValor =
+    localidadSeleccionada.options[localidadSeleccionadaIndice].value;
+
+  cPostalUsuario = localidadSeleccionadaValor;
+  document.getElementById("c_postal").value = cPostalUsuario;
+
+  if (cPostalUsuario[0] + cPostalUsuario[1] == "02") {
+    document.getElementById("provincia").value = "Albacete";
+  }
+  if (cPostalUsuario[0] + cPostalUsuario[1] == "13") {
+    document.getElementById("provincia").value = "Ciudad Real";
+  }
+  if (cPostalUsuario[0] + cPostalUsuario[1] == "16") {
+    document.getElementById("provincia").value = "Cuenca";
+  }
+  if (cPostalUsuario[0] + cPostalUsuario[1] == "19") {
+    document.getElementById("provincia").value = "Guadalajara";
+  }
+  if (cPostalUsuario[0] + cPostalUsuario[1] == "45") {
+    document.getElementById("provincia").value = "Toledo";
+  }
+}
+
 function enviarDatos() {
   // Se definen las variables.
   var nombreUsuario,
@@ -8,7 +65,7 @@ function enviarDatos() {
     domicilioUsuario,
     localidadUsuario,
     provinciaUsuario,
-    cPostalUsario,
+    cPostalUsuario,
     nHijosUsuario,
     tratamiento,
     aficiones = "",
@@ -16,7 +73,7 @@ function enviarDatos() {
     datosDomicilio,
     datosAficiones;
 
-  // Se extraen las variables tipo texto.
+  // Se extraen las variables tipo texto o número.
   nombreUsuario = document.getElementById("nombre").value;
   apellido1Usuario = document.getElementById("apellido1").value;
   apellido2Usuario = document.getElementById("apellido2").value;
@@ -24,15 +81,23 @@ function enviarDatos() {
   fechaNacimientoUsuario = document.getElementById("fecha_nacimiento").value;
   domicilioUsuario = document.getElementById("domicilio").value;
   nHijosUsuario = document.getElementById("hijos").value;
+  cPostalUsuario = document.getElementById("c_postal").value;
+  provinciaUsuario = document.getElementById("provincia").value;
+
+  // Se extrae la localidad (desplegable).
+  localidadSeleccionada = document.getElementById("localidad");
+  localidadSeleccionadaIndice = localidadSeleccionada.selectedIndex;
+  localidadUsuario =
+    localidadSeleccionada.options[localidadSeleccionadaIndice].text;
 
   // Se extraen las variables de tipo radio (género).
   // Según el género elegido cambiará la forma en la que nos dirigiremos al usuario.
 
   if (document.getElementById("hombre").checked) {
-    tratamiento = document.getElementById("hombre").value;
+    tratamiento = document.getElementById("hombre").value + "";
   }
   if (document.getElementById("mujer").checked) {
-    tratamiento = document.getElementById("mujer").value;
+    tratamiento = document.getElementById("mujer").value + "";
   }
   if (document.getElementById("otro").checked) {
     tratamiento = document.getElementById("otro").value;
@@ -64,7 +129,6 @@ function enviarDatos() {
   if (nHijosUsuario != 0) {
     datosPersonales =
       tratamiento +
-      " " +
       nombreUsuario +
       " " +
       apellido1Usuario +
@@ -101,7 +165,7 @@ function enviarDatos() {
     " (" +
     provinciaUsuario +
     ") con código postal " +
-    cPostalUsario +
+    cPostalUsuario +
     " .";
 
   datosAficiones =

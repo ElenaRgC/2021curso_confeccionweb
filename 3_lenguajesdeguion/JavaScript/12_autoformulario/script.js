6b75c924c27bc653804dcdca277ede4689ca5fc6
ElenaRgC <95099947+ElenaRgC@users.noreply.github.com>
2022-02-14 20:10:10 +0100

    Finished new form

--- 3_lenguajesdeguion/JavaScript/12_autoformulario/script.js
+++ 3_lenguajesdeguion/JavaScript/12_autoformulario/script.js
@@ -19,7 +19,33 @@ var nombreUsuario,
 var passwordUsuario,
   passwordSaved,
   nIntentos = 0,
-  numeroUsuario;
+  Usuario,
+  numeroUsuario,
+  passwords = new Array();
+
+passwords = ["abc123", "m1contrasena", "iniciarsesi0n"];
+
+// Array = Nº Usuario, Apellido 1, Apellido 2, Nombre, DNI, Dirección
+var usu1 = new Array();
+var usu2 = new Array();
+var usu3 = new Array();
+usu1 = [1, "Barragán", "Granados", "Luis", "92131280L", "Avda. Alameda, 86"];
+usu2 = [
+  2,
+  "Sánchez",
+  "Camacho",
+  "Nazario",
+  "52762149B",
+  "Calle Comandante Izarduy, 61",
+];
+usu3 = [
+  3,
+  "Arriaga",
+  "Menéndez",
+  "Piedad",
+  "02831435C",
+  "Plaza Extramuros, 68",
+];
 
 var i = 0,
   j = 0;
@@ -27,27 +53,63 @@ var i = 0,
 // Página de log-in.
 
 function iniciarSesion() {
+  Usuario = document.getElementById("usuario").value;
+  passwordUsuario = document.getElementById("password").value;
+  localStorage.setItem("Usuario", Usuario);
+
+  // Comprobamos que el número de usuario coincida con la posición de la array passwords.
+  // Como la array empieza en 0 pero los usuarios en 1, restamos una posición.
+  for (i = 0; i < 3; i++) {
+    if (Usuario - 1 == i) {
+      passwordSaved = passwords[i];
+    }
+  }
+
   if (nIntentos > 1) {
     document.getElementById("mensaje").innerHTML =
       "Ha introducido la contraseña incorrecta 3 veces.";
     document.getElementById("inicio").disabled = true;
   }
 
-  passwordSaved = "abc123";
-  passwordUsuario = document.getElementById("password").value;
-
   if (passwordSaved == passwordUsuario) {
     document.getElementById("mensaje").innerHTML = "Acceso permitido.";
     nIntentos = 3;
+    location.href = "formulario.html";
   } else {
     document.getElementById("mensaje").innerHTML =
       "Ha introducido una contraseña incorrecta.";
     nIntentos = nIntentos + 1;
   }
-
-  numeroUsuario = document.getElementById("usuario").value;
 }
+
 // Página de formulario
+
+function cargarUsuario() {
+  numeroUsuario = localStorage.getItem("Usuario");
+
+  if (numeroUsuario == 1) {
+    document.getElementById("apellido1").value = usu1[1];
+    document.getElementById("apellido2").value = usu1[2];
+    document.getElementById("nombre").value = usu1[3];
+    document.getElementById("dni").value = usu1[4];
+    document.getElementById("domicilio").value = usu1[5];
+  }
+  if (numeroUsuario == 2) {
+    document.getElementById("apellido1").value = usu2[1];
+    document.getElementById("apellido2").value = usu2[2];
+    document.getElementById("nombre").value = usu2[3];
+    document.getElementById("dni").value = usu2[4];
+    document.getElementById("domicilio").value = usu2[5];
+  }
+
+  if (numeroUsuario == 3) {
+    document.getElementById("apellido1").value = usu3[1];
+    document.getElementById("apellido2").value = usu3[2];
+    document.getElementById("nombre").value = usu3[3];
+    document.getElementById("dni").value = usu3[4];
+    document.getElementById("domicilio").value = usu3[5];
+  }
+}
 
 function comprobarDNI() {
   var letraDNI,
@@ -57,14 +119,17 @@ function comprobarDNI() {
 
   dniUsuario = document.getElementById("dni").value;
 
+  //Extraemos la letra del DNI, que se encuentra en la novena posición.
   letraDNI = dniUsuario[8];
 
+  // El input es de tipo texto, pero tenemos que convertirlo a número para poder dividirlo entre 23.
   for (i = 0; i < dniUsuario.length - 1; i++) {
     numeroDNI = numeroDNI + dniUsuario[i];
   }
 
   resto = Number(numeroDNI) % 23;
 
+  // Si la letra obtenida no se corresponde con la que se ha introducido, añadimos un estilo diferente al cuadro de texto.
   if (letras[resto] != letraDNI) {
     document.getElementById("dni").classList.add("rojo");
   } else {
@@ -78,14 +143,19 @@ function codigoPostal() {
     localidadSeleccionadaIndice,
     localidadSeleccionadaValor;
 
+  // Extraemos el indice y el valor de la localidad seleccionada.
   localidadSeleccionada = document.getElementById("localidad");
   localidadSeleccionadaIndice = localidadSeleccionada.selectedIndex;
   localidadSeleccionadaValor =
     localidadSeleccionada.options[localidadSeleccionadaIndice].value;
 
+  // Al haber definido los valores en el HTML, el valor será directamente el código postal asociado a esa localidad.
   cPostalUsuario = localidadSeleccionadaValor;
+
+  // Escribimos en el cuadro de texto el valor obtenido.
   document.getElementById("c_postal").value = cPostalUsuario;
 
+  // Decidimos la provincia en base a los dos primeros dígitos del código postal.
   cPostalInicio = cPostalUsuario[0] + cPostalUsuario[1];
 
   if (cPostalInicio == "02") {
@@ -106,35 +176,42 @@ function codigoPostal() {
 }
 
 function quitarHijos() {
+  // En el caso de que el usuario se equivoque en el número de hijos, se eliminan los cuadros de texto.
   document.getElementById("divisorHijos").innerHTML = "";
 }
 
 function anadirHijos() {
-  nHijosUsuario = document.getElementById("hijos").value;
+  // Si el número de hijos no cambia, NO crea nuevas casillas.
+  // Esto es necesario porque cambiar el foco sin cambiar el número ejecuta esta función, pero no ejecuta la función de limpieza quitarHijos que solo funciona onChange.
+  if (document.getElementById("hijos").value != nHijosUsuario) {
+    nHijosUsuario = document.getElementById("hijos").value;
 
-  if (nHijosUsuario != 0) {
-    // Solo se crea un div nuevo una vez, no cada vez que cambiemos la cantidad de hijos.
-    if (j == 0) {
-      divHijos = document.createElement("div");
-      divHijos.setAttribute("id", "divisorHijos");
-      document.getElementById("familia").appendChild(divHijos);
-      j = j + 1;
-    }
+    if (nHijosUsuario != 0) {
+      // Solo se crea un div nuevo una vez, no cada vez que cambiemos la cantidad de hijos.
+      if (j == 0) {
+        divHijos = document.createElement("div");
+        divHijos.setAttribute("id", "divisorHijos");
+        document.getElementById("familia").appendChild(divHijos);
+        j = j + 1;
+      }
 
-    for (i = 0; i < nHijosUsuario; i++) {
-      k = i + 1;
-      nuevoHijoLabel = document.createElement("label");
-      nuevoHijoLabel.setAttribute("for", "hijo" + i);
-      nuevoHijoLabel.innerHTML = "Nombre y apellidos de hijo " + k + ": ";
-      nuevoHijoInput = document.createElement("input");
-      nuevoHijoInput.setAttribute("type", "text");
-      nuevoHijoInput.setAttribute("id", "hijo" + i);
-      divHijos.appendChild(nuevoHijoLabel);
-      divHijos.appendChild(nuevoHijoInput);
-      divHijos.innerHTML += "<br>";
+      // Por cada hijo nuevo creamos una label y un input, que añadimos al contenedor divisorHijos.
+      for (i = 0; i < nHijosUsuario; i++) {
+        k = i + 1;
+        nuevoHijoLabel = document.createElement("label");
+        nuevoHijoLabel.setAttribute("for", "hijo" + i);
+        nuevoHijoLabel.innerHTML = "Nombre y apellidos de hijo " + k + ": ";
+        nuevoHijoInput = document.createElement("input");
+        nuevoHijoInput.setAttribute("type", "text");
+        nuevoHijoInput.setAttribute("id", "hijo" + i);
+        divHijos.appendChild(nuevoHijoLabel);
+        divHijos.appendChild(nuevoHijoInput);
+        divHijos.innerHTML += "<br>";
+      }
+    } else {
+      // Si el número de hijos es 0, eliminamos los cuadros de texto.
+      document.getElementById("divisorHijos").innerHTML = "";
     }
-  } else {
-    document.getElementById("divisorHijos").innerHTML = "";
   }
 }
 

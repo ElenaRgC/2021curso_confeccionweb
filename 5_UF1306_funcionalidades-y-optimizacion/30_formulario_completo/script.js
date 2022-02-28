ae4d98366c174a70f7509bb5ddbb501a13b584d4
ElenaRgC <95099947+ElenaRgC@users.noreply.github.com>
2022-02-28 20:08:07 +0100

    New functions

--- 5_UF1306_funcionalidades-y-optimizacion/30_formulario_completo/script.js
+++ 5_UF1306_funcionalidades-y-optimizacion/30_formulario_completo/script.js
@@ -41,6 +41,29 @@ function validarCampoDNI(elemento, mensaje) {
   }
 }
 
+function validarAficiones(mensaje) {
+  var opciones = document.getElementsByName("aficion");
+  opcion = new Array();
+
+  for (i = 0; i < opciones.length; i++) {
+    if (opciones[i].checked) {
+      opcion[i] = opciones[i].value;
+    }
+  }
+
+  // Solo entrará en el bucle si hay al menos un elemento,
+  // es decir, si opción tiene longitud.
+  if (opcion.length) {
+    document.getElementById(mensaje).className = "textoOK";
+    document.getElementById(mensaje).innerHTML =
+      "Campo rellenado correctamente.";
+  } else {
+    document.getElementById(mensaje).className = "textoError";
+    document.getElementById(mensaje).innerHTML =
+      "Debe rellenar este campo correctamente.";
+  }
+}
+
 function validarSexo(mensaje) {
   var opciones = document.getElementsByName("sexo");
 
@@ -58,5 +81,25 @@ function validarSexo(mensaje) {
     document.getElementById(mensaje).className = "textoError";
     document.getElementById(mensaje).innerHTML =
       "Debe rellenar este campo correctamente.";
+  }
+}
+
+function limpiarFormulario() {
+  // Definimos el div que contiene el formulario pero no los botones.
+  // O borraríamos el nombre de los botones también.
+  var formulario = document.getElementById("form");
+  var cuadrosTexto = formulario.getElementsByTagName("input");
+  for (i = 0; i < cuadrosTexto.length; i++) {
+    cuadrosTexto[i].value = "";
+  }
+
+  var aficion = document.getElementsByName("aficion");
+  for (i = 0; i < aficion.length; i++) {
+    aficion[i].checked = false;
+  }
+
+  var sexo = document.getElementsByName("sexo");
+  for (i = 0; i < sexo.length; i++) {
+    sexo[i].checked = false;
   }
 }

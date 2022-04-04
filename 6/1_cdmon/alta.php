aa0477a22f0f8573fa5c53ff4c490b1d40c582a4
ElenaRgC <95099947+ElenaRgC@users.noreply.github.com>
2022-04-04 22:23:23 +0200

    New content

--- 6/1_cdmon/alta.php
+++ 6/1_cdmon/alta.php
@@ -1,34 +1,3 @@
-Repaso Tema 2
-Joaquín Rubio
-•
-31 mar (Última modificación: 1 abr)
-100 puntos
-Sigue las instrucciones del archivo adjunto y súbelo con las soluciones y los documentos que solicita.
-
-k Repaso Tema 2 2022.odt
-OpenOffice Writer
-
-alta.html
-HTML
-
-alta.php
-HTML
-Comentarios de la clase
-Tu trabajo
-Entregado
-
-ALTA.html
-HTML
-
-alta.php
-Archivo desconocido
-
-comprobar.php
-Archivo desconocido
-
-Repaso Tema 2
-Documentos de Google
-Comentarios privados
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
@@ -44,7 +13,7 @@ $usuario= ($_POST["usu"]);
 $clave= ($_POST["clave"]);
 $nya= ($_POST["nya"]);
 
-$mysqli = new mysqli("localhost", "root", "Sepecam2020-21", "ejemplo");
+$mysqli = new mysqli("localhost","myelenarc","F4g0YnT7","ejemplopublica04");
 
 
 //Conecta con el servidor
